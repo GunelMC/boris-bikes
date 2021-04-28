0668890eaa2594ec3a9820b0dbccaf521224da75
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-04-28 19:57:09 +0100

    made docking stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike)}
-end 
+  it { is_expected.to respond_to(:release_bike) }
+
+  it 'checks if the bike works' do 
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike.working?).to eq true
+  end 
+
+end
