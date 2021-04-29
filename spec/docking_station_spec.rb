840e9a72b9d8334bb611f472e2234b7a90bc6c14
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-04-29 20:40:06 +0100

    raised exception error when no bikes available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,19 +4,31 @@ require 'bike'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
-  it 'checks if the bike works' do 
-    docking_station = DockingStation.new
-    bike = docking_station.release_bike
-    expect(bike.working?).to eq true
-  end 
+  # it 'checks if the bike works' do 
+  #   docking_station = DockingStation.new
+  #   bike = docking_station.release_bike
+  #   expect(bike.working?).to eq true
+  # end 
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'shows docked bike' do
     bike = Bike.new
-    docking_station = DockingStation.new
-    docking_station.dock(bike)
-    expect(docking_station.bike).to eq bike
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
+
+    it 'releases a bike when asked for one' do
+    # Arrange
+    bike = Bike.new
+    # Act 
+    subject.dock(bike)
+    #Assert
+    expect(subject.release_bike).to eq(bike)
+  end 
+
+  it 'raises an error message when there are no bikes' do
+    expect { subject.release_bike }.to raise_error('No bikes in the station')
+  end 
 
 end
